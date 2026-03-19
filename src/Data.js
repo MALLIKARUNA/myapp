67d6371a56183cbde000e9fd1c9bb7ff3a43cc0f
mallikarjunalyal262-boop <mallikarjunalyal262@gmail.com>
2026-03-19 13:34:16 +0530

    feat: Add initial React application setup including `index.js`, `App.js`, and `Data.js` files.

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+
+// const data = [{
+//   name: "mallikarjun alyal",
+//   age: 90,
+//   city: "hubli",
+//   state: "karnataka",
+//   country: "india",
+//   pincode: 580020,
+
+// }
+// ]
+// export default data;
